134ec6a07189398912b0d0855d16ae335ebb4ffa
Breana Lo <breana456l@gmail.com>
2021-11-26 11:44:30 -0500

    Interface outline made, jar made

--- AlsetEnterpriseMain.java
+++ AlsetEnterpriseMain.java
@@ -42,31 +42,156 @@ public class AlsetEnterpriseMain {
                     do{
                         //user menu here
                         System.out.println("Hello! What can we help you with?\n" 
-                             + "1: Valued Alset Customer \n" );
+                             + "1: View profile \n"
+                             + "2: Edit profile \n"
+                             + "3: View wallet \n"
+                             + "4: Edit wallet \n"
+                             + "5: View your vehicles \n"
+                             + "6: View service locations \n"
+                             + "7: View all Alset models \n" //inside add option to see model options as well
+                             + "8: Exit \n" );
                         while (!s.hasNextInt()) {
                             String input = s.next();
-                            System.out.println("INVALID INPUT\n*PLEASE ENTER A NUMBER 1-4*");
+                            System.out.println("INVALID INPUT\n*PLEASE ENTER A NUMBER 1-8*");
                         }
                         int c1 = s.nextInt();
-                        if(c1 == 4){
+                        if(c1 == 8){
                             run1 = 1;
                             break;
                         }
                         switch(c1){
                             case 1:
                             break;
+                            case 2:
+                            break;
+                            case 3:
+                            break;
+                            case 4:
+                            break;
+                            case 5:
+                            break;
+                            case 6:
+                            break;
+                            case 7:
+                            break;
                         }
                     }while(run1 == 0);
                     break;
-                case 2: //store manager menu here
-                    System.out.println("manager");
+                case 2: 
+                 //log in method
+                 System.out.print("enter Oracle user id: ");
+                 userID = s.next();
+                 System.out.print("enter Oracle password for " + userID + ": ");
+                 password = s.next();
+                 a.logOn(userID, password); //user is stuck in log on until log on is complete
+                 //when user is logged in, they can view the menu and make actions
+                 do{
+                    //store manager menu here
+                     System.out.println("Hello Store Manager! What can we help you with?\n" 
+                          + "1: View all customers \n" //view cards per customer inside 
+                          + "3: View prices \n"
+                          + "4: Edit prices \n" //edit prices if new repairs are added
+                          + "5: View repairs \n"
+                          + "6: Edit repairs \n" //managers can add aviable repairs, if they hire a new employee who can handle a new repair type
+                          + "7: View all Alset models \n" //inside add option to see model options as well
+                          + "8: View Showroom information \n"
+                          + "9: Edit Showroom information \n"
+                          + "10: View service location information \n"
+                          + "11: Edit Showroom information \n" 
+                          + "12: Exit \n" );
+                     while (!s.hasNextInt()) {
+                         String input = s.next();
+                         System.out.println("INVALID INPUT\n*PLEASE ENTER A NUMBER 1-12*");
+                     }
+                     int c2 = s.nextInt();
+                     if(c2 == 12){
+                         run1 = 1;
+                         break;
+                     }
+                     switch(c2){
+                         case 1:
+                         break;
+                         case 2:
+                         break;
+                         case 3:
+                         break;
+                         case 4:
+                         break;
+                         case 5:
+                         break;
+                         case 6:
+                         break;
+                         case 7:
+                         break;
+                         case 8:
+                         break;
+                         case 9:
+                         break;
+                         case 10:
+                         break;
+                         case 11:
+                         break;
+                     }
+                 }while(run2 == 0);
                     break;
                 case 3: //product manager menu here
-                    System.out.println("product manager");
+                    //log in method
+                 System.out.print("enter Oracle user id: ");
+                 userID = s.next();
+                 System.out.print("enter Oracle password for " + userID + ": ");
+                 password = s.next();
+                 a.logOn(userID, password); //user is stuck in log on until log on is complete
+                 //when user is logged in, they can view the menu and make actions
+                 do{
+                    //store manager menu here
+                     System.out.println("Hello Product Manager! What can we help you with?\n" 
+                          + "1: View all customers \n" //view cards per customer inside 
+                          + "3: View prices \n"
+                          + "4: Edit prices \n" //edit prices if products are changed
+                          + "5: View repairs \n"
+                          + "6: View all Alset models \n" //inside add option to see model options as well
+                          + "7: Edit Alset models"
+                          + "8: View Showroom information \n"
+                          + "9: View service location information \n"
+                          + "10: Issue maintenance \n"
+                          + "11: Issue recall \n"
+                          + "12: Exit \n" );
+                     while (!s.hasNextInt()) {
+                         String input = s.next();
+                         System.out.println("INVALID INPUT\n*PLEASE ENTER A NUMBER 1-12*");
+                     }
+                     int c3 = s.nextInt();
+                     if(c3 == 12){
+                         run3 = 1;
+                         break;
+                     }
+                     switch(c3){
+                         case 1:
+                         break;
+                         case 2:
+                         break;
+                         case 3:
+                         break;
+                         case 4:
+                         break;
+                         case 5:
+                         break;
+                         case 6:
+                         break;
+                         case 7:
+                         break;
+                         case 8:
+                         break;
+                         case 9:
+                         break;
+                         case 10:
+                         break;
+                         case 11:
+                         break;
+                     }
+                 }while(run3 == 0);
                     break;
             }
-
-
         }while (run == 0);
 
     }
