d63332a4dbfab653cc8776eb5a1a1ae1a488712a
Breana Lo <breana456l@gmail.com>
2021-11-26 12:52:36 -0500

    methods started

--- AlsetMethods.java
+++ AlsetMethods.java
@@ -26,4 +26,137 @@ public class AlsetMethods {
             logOn(id, pass);
         }
     }
+//----------------------------------------------------------------------------------------------------------------
+    public void viewCustomer(){
+        String p = "";
+        ResultSet answer;
+        
+        try (
+                Connection con = DriverManager.getConnection("jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241");
+                Statement s=con.createStatement();
+            ) {
+                Scanner myScanner = new Scanner(System.in);
+                System.out.print("Input the email affiliated to your account: ");
+                String emailForSearch = myScanner.next();
+                p = "select firstname, lastname, email, address from customer where email like '%" + emailForSearch + "%'";   
+
+                answer = s.executeQuery(p);
+                System.out.println();
+
+                while(answer.next()){
+                    System.out.println(answer.getString("firstname") + " " + answer.getString("lastname") 
+                    + " \n" + answer.getString("email") + " \n" + answer.getString("address") + " \n" 
+                    + "---------------------------------------"); // FINISH
+                }
+                s.close();
+                con.close();
+            }
+            catch (Exception sqle){
+                System.out.println(sqle.toString());
+            }
+    }
+    public void viewAllCustomer(){
+        String p = "";
+        ResultSet answer;
+        
+        try (
+                Connection con = DriverManager.getConnection("jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241");
+                Statement s=con.createStatement();
+            ) {
+                p = "select firstname, lastname, email, address from customer where customer_id > 0";   
+
+                answer = s.executeQuery(p);
+                System.out.println();
+
+                while(answer.next()){
+                    System.out.println(answer.getString("firstname") + " " + answer.getString("lastname") 
+                    + " \n" + answer.getString("email") + " \n" + answer.getString("address") + " \n" 
+                    + "---------------------------------------"); // FINISH
+                }
+                s.close();
+                con.close();
+            }
+            catch (Exception sqle){
+                System.out.println(sqle.toString());
+            }
+    }
+    public void editCustomer(){
+
+    }
+//----------------------------------------------------------------------------------------------------------------
+    public String viewCard(){
+
+        return "";
+    }
+    public String viewAllCard(){
+
+        return "";
+    }
+    public void editCard(){
+
+    }
+//----------------------------------------------------------------------------------------------------------------
+    public String viewVehicle(){
+
+        return "";
+    }
+    public String viewAllVehicle(){
+
+        return "";
+    }
+    public void editVehicle(){
+
+    }
+//----------------------------------------------------------------------------------------------------------------
+    public String viewModel(){
+
+        return "";
+    }
+    public String viewAllModel(){
+
+        return "";
+    }
+    public void editModel(){
+
+    }
+//----------------------------------------------------------------------------------------------------------------
+    public String viewAllServiceLocations(){
+
+        return "";
+    }
+    public void editServiceLocations(){
+
+    }
+//----------------------------------------------------------------------------------------------------------------
+    public String viewAllShowroom(){
+
+        return "";
+    }
+    public void editShowroom(){
+
+    }
+//----------------------------------------------------------------------------------------------------------------
+    public String viewPrice(){
+
+        return "";
+    }
+    public String viewAllPrice(){
+
+        return "";
+    }
+    public void editPrice(){
+
+    }
+//----------------------------------------------------------------------------------------------------------------
+    public String viewRepairs(){
+
+        return "";
+    }
+    public String viewAllRepairs(){
+
+        return "";
+    }
+    public void editRepairs(){
+
+    }
 }
